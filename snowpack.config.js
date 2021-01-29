80750ff83e97d86669574f814e5d72a781eb0eeb
Roma K <804157+romakuchukov@users.noreply.github.com>
2021-01-29 13:49:09 -0500

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,24 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    /* ... */
+    "src": "/",
+    "public": "/"
+  },
+  exclude: ['**/node_modules/**/*'],
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
