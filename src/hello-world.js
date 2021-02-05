6b50eb87dcf2b6254cb8b946899e69a801281a77
Roma K <804157+romakuchukov@users.noreply.github.com>
2021-02-04 22:31:08 -0500

    added css and html to the template

--- src/hello-world.js
+++ src/hello-world.js
@@ -2,3 +2,22 @@
 export function helloWorld() {
   console.log('Hello World!');
 }
+
+
+// // >> && || === <<
+
+// function helloWorld()
+//                     {
+//   if                (
+//     condition
+//                     )
+//                     {
+//     Statement
+//     // if the condition is true
+//                     }
+//   else
+//                     {
+//     Statement
+//     // if the condition is true
+//                     }
+//                     }
