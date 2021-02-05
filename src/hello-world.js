d0eef6000e3a30286bb974e7bdd3a07055256f30
Roma K <804157+romakuchukov@users.noreply.github.com>
2021-02-04 23:20:43 -0500

    removed cruft

--- src/hello-world.js
+++ src/hello-world.js
@@ -2,22 +2,3 @@
 export function helloWorld() {
   console.log('Hello World!');
 }
-
-
-// // >> && || === <<
-
-// function helloWorld()
-//                     {
-//   if                (
-//     condition
-//                     )
-//                     {
-//     Statement
-//     // if the condition is true
-//                     }
-//   else
-//                     {
-//     Statement
-//     // if the condition is true
-//                     }
-//                     }
